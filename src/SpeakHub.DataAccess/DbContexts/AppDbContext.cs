a1f1f226935f17753066fd74253a82c93c2dd537
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-08-07 12:17:40 +0500

    all entities created

---
+++ src/SpeakHub.DataAccess/DbContexts/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpeakHub.Domain.Entities.Admins;
+using SpeakHub.Domain.Entities.Tweets;
+using SpeakHub.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakHub.DataAccess.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+            //Database.Migrate();
+            //Database.EnsureCreated();
+        }
+        public virtual DbSet<User> Users { get; set; } = default!;
+        public virtual DbSet<UserProfile> UserProfiles { get; set; } = default!;
+        public virtual DbSet<Admin> Admins { get; set; } = default!;
+        public virtual DbSet<Tweet> Tweets { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.ApplyConfiguration(new SuperAdminConfiguration());
+        }
+    }
+}
---
+++ src/SpeakHub.Domain/Entities/Tweets/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using SpeakHub.Domain.Common;
+using SpeakHub.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakHub.Domain.Entities.Tweets
+{
+    public class Tweet : Auditable
+    {
+        public string TweetText { get; set; } = string.Empty;
+        public string EditTweetText { get; set; } = string.Empty;
+        public long UserId { get; set; }
+        public virtual User User { get; set; } = default!;
+    }
+}
